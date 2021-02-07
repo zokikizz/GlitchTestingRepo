93aca0c19138212da4fe450f12c5b8b509d100a2
Zoran Pecic <zoran.pecic@MRSC02PG1T3G3QN-3.local>
2021-02-07 17:51:06 +0100

    Adding typeorm and db stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+import { NotesModule } from './notes/notes.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    NotesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
